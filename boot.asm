6655791eb6e2f3c294d6059ae4c3702795bb0d1a
ThatGuyFromLol <lubiepcty59@gmail.com>
2026-03-05 03:08:57 +0100

    Update boot.asm
    gotowe gdt

--- boot.asm
+++ boot.asm
@@ -23,16 +23,35 @@ done:
 cli
 hlt ;stop
 
-gdt_start:       ;Global descyptor table
+msg: 
+db 'hello world boot',0
+
+gdt_start:       
+;Global descyptor table
  db 0x00000000
  db 0x00000000
  ; code segment deskryptor
- dw 0xFFFF ;limit
- dw 0x0000 ;baza
- db 0x00   ;baza
- db 10011010b
-msg: 
-db 'hello world boot',0
+ dw 0xFFFF    ;limit
+ dw 0x0000    ;baza
+ db 0x00      ;baza
+ db 10011010b ;Acces byte
+ db 11001111b ;flagi 
+ db 0x00      ;baza
+
+ ; data segment deskryptor
+ dw 0xFFFF    ;limit
+ dw 0x0000    ;baza
+ db 0x00      ;baza
+ db 10010010b ;Acces byte
+ db 11001111b ;flagi 
+ db 0x00      ;baza
+
+gdt_end:
+
+gdt_descryptor:
+
+dw gdt_end - gdt_start - 1 ;wielkość gdy -1
+dd gdt_start
 
 times 510 - ($-$$) db 0
 
