d1e51c0c721e43b9e72fd65649447e89a016d1cf
ThatGuyFromLol <lubiepcty59@gmail.com>
2026-06-07 05:18:09 +0200

    Update uefi_boot.asm

--- Bootloders/uefi_boot.asm
+++ Bootloders/uefi_boot.asm
@@ -1,12 +1,12 @@
 bits 64
 section .text
- global _start
+global _start
 
 _start:
 sub rsp, 40
 mov [image_handle], rcx
 mov [sys_table], rdx
-
+mov r8, cr3
 mov rbx, [rdx + 64] ;wskaźnik conout
 mov rcx, rbx        ; komenda print
 lea rdx, [rel hello_str]
